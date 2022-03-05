d0cfa477c2d0ac3d9f48ca696b6a32f62b33d522
milselarch <milselarch@gmail.com>
2022-03-05 15:57:09 +0800

    add NFC library

--- subapps/charles_nfc/app/src/main/java/com/example/charles_nfc/MainActivity.java
+++ subapps/charles_nfc/app/src/main/java/com/example/charles_nfc/MainActivity.java
@@ -4,13 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import com.github.skjolber.ndef.externaltype.AndroidApplicationRecord;
+import android.nfc.NfcAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            mTextView.setText("NFC is disabled.");
+        } else {
+            mTextView.setText(R.string.explanation);
+        }
+
+        handleIntent(getIntent());
     }
 }
